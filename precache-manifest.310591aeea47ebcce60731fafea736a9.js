3b999972fbc3a09aadab1ff376e984e70610a11a
MillanUka <41311473+MillanUka@users.noreply.github.com>
2019-11-16 10:50:17 +1300

    Updates

---
+++ precache-manifest.310591aeea47ebcce60731fafea736a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ce10abd5c99b008218ae0b1bcbd8d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941e863e311791e2cf3f",
+    "url": "/static/css/main.581cc4d9.chunk.css"
+  },
+  {
+    "revision": "aef5f4bfee42ebc2dce6",
+    "url": "/static/js/2.7e131244.chunk.js"
+  },
+  {
+    "revision": "941e863e311791e2cf3f",
+    "url": "/static/js/main.107ac019.chunk.js"
+  },
+  {
+    "revision": "23e11197eb0ec2d8596f",
+    "url": "/static/js/runtime-main.d17f8506.js"
+  }
+]);
